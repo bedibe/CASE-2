05b3949e6c7b52cf2b4136459d2f29aeda8c7fff
Kesia <112557978+kesia8@users.noreply.github.com>
2023-03-01 09:01:05 -0300

    Update Destaques.jsx

--- frontend/src/components/Destaques.jsx
+++ frontend/src/components/Destaques.jsx
@@ -6,7 +6,7 @@ function Destaques() {
         <Row className='mt-2'>
             <Col className='mt-2' sm="12" md="4">
                 <Card>
-                    <Card.Img variant="top" src="https://via.placeholder.com/300" />
+                    <Card.Img variant="top" src="https://wtourtravel.com/wp-content/uploads/2021/12/WTOUR-Fotos-de-Imagens-1-1.png" />
                     <Card.Body>
                         <Card.Title>Fácil de usar</Card.Title>
                         <Card.Text>
@@ -20,7 +20,7 @@ function Destaques() {
             </Col>
             <Col className='mt-2' sm="12" md="4">
                 <Card>
-                    <Card.Img variant="top" src="https://via.placeholder.com/300" />
+                    <Card.Img variant="top" src="https://www.enjoytrip.com.br/wp-content/uploads/2021/05/ilhas-maldivas.png" />
                     <Card.Body>
                         <Card.Title>Rápido e confiável</Card.Title>
                         <Card.Text>
@@ -34,7 +34,7 @@ function Destaques() {
             </Col>
             <Col className='mt-2' sm="12" md="4">
                 <Card>
-                    <Card.Img variant="top" src="https://via.placeholder.com/300" />
+                    <Card.Img variant="top" src="https://www.queroviajarmais.com/wp-content/uploads/2020/02/por-onde-passear-em-santo-domingo.jpg" />
                     <Card.Body>
                         <Card.Title>Suporte personalizado</Card.Title>
                         <Card.Text>
