46b70faf3937efdb193602f813d4b237b0e8ced8
Ronald Da Silva <112557309+Ronald-02@users.noreply.github.com>
2023-03-03 01:36:33 -0300

    Update Funcionalidades.jsx

--- frontend/src/pages/Funcionalidades.jsx
+++ frontend/src/pages/Funcionalidades.jsx
@@ -1,35 +1,15 @@
-import ListaFuncionalidades from "../components/ListaFuncionalidades"
-import Container from 'react-bootstrap/Container'
-import Banners from "../components/Banners"
-import { useEffect, useState } from "react"
-import CmsApi from '../api/CmsApi'
+import React from 'react';
+import '../assets/css/Lista.css';
+import TodoList from '../components/TodaLista';
+import BootstrapCarousel from '../components/Carousel';
 
-function Funcionalidades() {
-    const [funcionalidades, setFuncionalidades] = useState([])
-
-    useEffect(() => {
-        async function fetchFuncionalidades() {
-            const response = await CmsApi().getFuncionalidades()
-            if(!response.ok) {
-                alert('Erro ao carregar funcionalidades')
-                return
-            }
-            const funcionalidades = await response.json()
-            setFuncionalidades(funcionalidades.data)
-        }
-
-        fetchFuncionalidades()
-    }, [])
-
-    return (
-        <>
-        <Banners />
-        <Container className="conteudo-margin">
-            <h1>To-Do</h1>
-            <ListaFuncionalidades funcionalidades={funcionalidades} />
-        </Container>
-        </>
-    );
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+      <BootstrapCarousel/>
+    </div>
+  );
 }
 
-export default Funcionalidades
+export default App;
